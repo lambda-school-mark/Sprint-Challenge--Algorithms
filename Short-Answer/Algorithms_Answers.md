577b8ab6f3204f607b150e9abeac54c195029ebc
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-09-18 15:40:19 -0400

    mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,15 +1,13 @@
-#### Please add your answers to the ***Analysis of  Algorithms*** exercises here.
+#### Please add your answers to the **_Analysis of Algorithms_** exercises here.
 
 ## Exercise I
 
-a)
+a) O(n) its linear because it is based on the input of n and grow at same rate
 
+b) 0(n logn) first loop is based on n, but the second loop increases complexity also based on n
 
-b)
-
-
-c)
+c) O(n) a recursive function is linear unless its complexity is increased, but it grows at same rate each time it is called
 
 ## Exercise II
 
-
+Start at midpoint and then continue finding the midpoint of top or bottom, by ruling out half on each drop, you are minimizing the amount of both dropped and broken eggs. This means you would probably use a binary search, resulting in a time complexity of O(log n)
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,24 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    # check if less than two characters
+    if len(word) < 2:
+        # return 0 since impossible
+        # also stops the recurive cycle
+        return 0
+    # check if first two characters are 'th'
+    if word[:2] == 'th':
+        # if so, return one
+        # and iterate through next two characters
+        # by a recursive call
+        return 1 + count_th(word[1:])
+    else:
+        # if its not 'th' then continue to next set
+        # also by removing the first character
+        return count_th(word[1:])
+
+
+print(count_th("threethreethree"))
